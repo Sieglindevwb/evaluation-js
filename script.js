055281bf38f0e827d0e857126b81901c11ab8c6c
Sieglinde <sieglindevwb@gmail.com>
2023-11-24 10:28:45 +0100

    Add color and addEventListener to button

--- script.js
+++ script.js
@@ -9,14 +9,33 @@ const drinks = [
   { name: 'Lemonade', price: 3 },
 ];
 
+const random = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
+const randomColor = () => {
+  const red = random(255);
+  const green = random(255);
+  const blue = random(255);
+  return 'rgb(' + red + ',' + green + ',' + blue + ')';
+};
+
 drinks.forEach((drink) => {
   const menuDrinks = document.querySelector('.container');
+  const colorButton = randomColor();
 
   buttonDrinks = document.createElement('button');
+  buttonDrinks.classList.add('buttonDrinks');
   buttonDrinks.textContent = `${drink.name} €${drink.price}`;
+  buttonDrinks.style.backgroundColor = colorButton;
+
+  buttonDrinks.addEventListener('click', () => {
+    console.log('button clicked!');
+  });
 
   menuDrinks.appendChild(buttonDrinks);
 });
 
-//TODO: List of not cocktails and cocktails
-//TODO: Generate buttons, for each one with a different color. They all represent a drink.
+//DONE: List of not cocktails and cocktails
+//DONE: Generate buttons, for each one with a different color. They all represent a drink.
+//DONE: Each button other color
