2d29ed055fd7f5fe2859ae52327d7f6309369701
Sieglinde <sieglindevwb@gmail.com>
2023-11-24 13:49:44 +0100

    Add selected items in list

--- script.js
+++ script.js
@@ -8,6 +8,42 @@ const drinks = [
   { name: 'Ginger ale', price: 3, isCocktail: false, type: 'soft drink' },
   { name: 'Lemonade', price: 3, isCocktail: false, type: 'soft drink' },
 ];
+const buttonList = document.querySelector('#selectContent');
+
+async function getCocktailData(drink) {
+  try {
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink.name}`
+    );
+    const data = await response.json();
+    if (data.drinks && data.drinks.length > 0) {
+      const cocktail = data.drinks[0];
+      return {
+        imageURL: cocktail.strDrinkThumb,
+      };
+    } else {
+      console.log(`Cocktail ${drink.name} not found in the API`);
+      return null;
+    }
+  } catch {
+    console.error('Error fetching cocktail data:', error);
+    return null;
+  }
+}
+
+const displayCocktailContent = async (drink) => {
+  drink.type = 'cocktail';
+  const cocktailData = await getCocktailData(drink);
+
+  if (cocktailData) {
+    const cocktailImage = document.createElement('img');
+    cocktailImage.src = cocktailData.imageURL;
+
+    updateContent(cocktailImage);
+  } else {
+    alert('no picture for this cocktail');
+  }
+};
 
 const displayTeaContent = (drink) => {
   drink.type = 'tea';
@@ -39,7 +75,7 @@ const displaySoftDrinksContent = (drink) => {
 };
 
 const updateContent = (content) => {
-  const targetDiv = document.getElementById('softDrinkContent');
+  const targetDiv = document.getElementById('drinkContent');
 
   targetDiv.innerHTML = '';
   targetDiv.appendChild(content);
@@ -56,33 +92,79 @@ const randomColor = () => {
   return 'rgb(' + red + ',' + green + ',' + blue + ')';
 };
 
-drinks.forEach((drink) => {
-  const menuDrinks = document.querySelector('.container');
+const createButton = (drink) => {
   const colorButton = randomColor();
 
-  buttonDrinks = document.createElement('button');
+  const buttonDrinks = document.createElement('button');
   buttonDrinks.classList.add('buttonDrinks');
   buttonDrinks.textContent = `${drink.name} €${drink.price}`;
   buttonDrinks.style.backgroundColor = colorButton;
 
+  return buttonDrinks;
+};
+
+const addListClickListener = (buttonDrinks, drink) => {
   buttonDrinks.addEventListener('click', () => {
+    const buttonListItem = document.createElement('li');
+    buttonListItem.textContent = `Clicked selected ${drink.name} for €${drink.price}`;
+    buttonList.appendChild(buttonListItem);
+
     if (drink.isCocktail === true) {
       console.log('a cocktail');
     } else {
       console.log('not a cocktail');
     }
-  });
 
-  buttonDrinks.addEventListener('click', () => {
     if (drink.type === 'soft drink') {
       displaySoftDrinksContent(drink);
     } else if (drink.type === 'tea') {
       displayTeaContent(drink);
+    } else if (drink.type === 'cocktail') {
+      displayCocktailContent(drink);
     }
   });
+};
+
+drinks.forEach((drink) => {
+  const menuDrinks = document.querySelector('.container');
+  const buttonDrinks = createButton(drink);
+
+  addListClickListener(buttonDrinks, drink);
 
   menuDrinks.appendChild(buttonDrinks);
 });
+// drinks.forEach((drink) => {
+//   const menuDrinks = document.querySelector('.container');
+//   const colorButton = randomColor();
+
+//   buttonDrinks = document.createElement('button');
+//   buttonDrinks.classList.add('buttonDrinks');
+//   buttonDrinks.textContent = `${drink.name} €${drink.price}`;
+//   buttonDrinks.style.backgroundColor = colorButton;
+
+//   buttonDrinks.addEventListener('click', () => {
+//     const buttonListItem = document.createElement('li');
+//     buttonListItem.textContent = `Clicked selected ${drink.name} for €${drink.price}`;
+//     buttonList.appendChild(buttonListItem);
+//     if (drink.isCocktail === true) {
+//       console.log('a cocktail');
+//     } else {
+//       console.log('not a cocktail');
+//     }
+//   });
+
+//   buttonDrinks.addEventListener('click', () => {
+//     if (drink.type === 'soft drink') {
+//       displaySoftDrinksContent(drink);
+//     } else if (drink.type === 'tea') {
+//       displayTeaContent(drink);
+//     } else if (drink.type === 'cocktail') {
+//       displayCocktailContent(drink);
+//     }
+//   });
+
+//   menuDrinks.appendChild(buttonDrinks);
+// });
 
 //DONE: List of not cocktails and cocktails
 //DONE: Generate buttons, for each one with a different color. They all represent a drink.
@@ -91,6 +173,7 @@ drinks.forEach((drink) => {
 //DONE: make each drink in array a type
 //DONE: function when clicked on soft drink
 //DONE: Function that displays content for making tea
-//TODO: Function when clicking on a cocktail, a picture should appear
-//TODO: make a API request
-//TODO: Main Event listener function -> so when button is clicked prepare content based on type of drink
+//DONE: Function when clicking on a cocktail, a picture should appear
+//DONE: make a API request
+//DONE: Main Event listener function -> so when button is clicked prepare content based on type of drink
+//TODO: every drink that was clicked in a list
